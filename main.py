54bda18e11b65f1e2e2da0c45c79cb1b46059acd
Connor Hale <connor.a.h.h@gmail.com>
2018-10-31 22:14:08 -0700

    Added mute function based on user ID, added random image selector, and added new images to database

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 import time
 import sys
 import json
-#import pathlib from Path #used to handle windows / characters in file names
+import random
 from meme_images_dict import memes
 
 config_data = json.load(open('config.json'))
@@ -13,18 +13,28 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("Bot is ready")
-    print(memes)
+
+def check(msg):
+    if msg.channel.id == '1':
+        return False
+    return True
 
 @client.event
 async def on_message(message):
-    if message.content in memes:
-        await client.send_file(message.channel, memes.get(message.content))
-
-    elif message.author.id == "142425531801927680" and message.content == "*restart":
-        await client.close()
-        sys.exit(0)
+    if check(message):
+        """if message.author.id == "168877392729866240":
+            await client.delete_message(message)
+            await client.send_message(message.channel, "sorry, you can't talk right now")
+            """
+        if message.content in memes:
+            await client.send_file(message.channel, memes.get(message.content))
+        elif message.content =="*okhand":
+            string = "*okhand"
+            r = random.randint(1,7)
+            randomOk = string+str(r)
+            await client.send_file(message.channel, memes.get(randomOk))
+        elif message.author.id == "142425531801927680" and message.content == "*restart":
+            await client.close()
+            sys.exit(0)
 
 client.run(config_data['access_token'])
-# things to consider:
-#meme_images_dict to search for images
-
